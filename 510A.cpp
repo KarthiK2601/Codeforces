d34947a24b82067b8dda61b5e6786721d9a6e374
KarthiK2601 <59332492+KarthiK2601@users.noreply.github.com>
2021-05-03 13:01:51 +0530

    Update 510A.cpp

--- 510A.cpp
+++ 510A.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include<algorithm>
 #include<vector>
@@ -12,21 +13,38 @@ void drawHash(const int& m) {
 	std::cout << "\n";
 }
 
-void drawDotHash(const int& m) {
-	for (int i = 1; i < m; i++) {
+void drawDotHash(const int& n) {
+	for (int i = 1; i < n; i++) {
 		std::cout << ".";
 	}
 	std::cout << "#";
 	std::cout << "\n";
 }
 
-int main() {
+void drawHashDot(const int& p) {
+	std::cout << "#";
+	for (int i = 1; i < p; i++) {
+		std::cout << ".";
+	}
+	
+	std::cout << "\n";
+}
 
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	int n, m; std::cin >> n >> m; 
+	int n, m; std::cin >> n >> m;
 
-	
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 1) {
+			drawHash(m);
+			continue;
+		}
+		if (i % 4 == 0)
+			drawHashDot(m);
+		else
+			drawDotHash(m);
+	}
 
 }
